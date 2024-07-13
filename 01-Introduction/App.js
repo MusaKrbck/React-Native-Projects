bff46f424bf1c35cebc129fd5b6327c1f223ded4
MusaKrbck <musakarabacak129@gmail.com>
2024-07-13 22:18:08 +0300

    Initial Commit

---
+++ 01-Introduction/App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.musastil}>!Top Level!</Text>
+      <View style={styles.container}>
+        <Text style={styles.musastil}>TAM ORTA</Text>
+        <Button title='BUTONA BAS' color={'black'} ></Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  musastil: {
+    margin: 16,
+    borderWidth: 1,
+    borderColor: "blue",
+    borderRadius:20,
+    padding: 15
+  }
+});
