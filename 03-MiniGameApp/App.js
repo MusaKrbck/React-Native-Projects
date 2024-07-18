2f8742a025beaa8044a041f3ac293591f745a178
MusaKrbck <musakarabacak129@gmail.com>
2024-07-18 21:58:35 +0300

    Adding Some Styles

--- 03-MiniGameApp/App.js
+++ 03-MiniGameApp/App.js
@@ -1,33 +1,56 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
+import GameOverScreen from './screens/GameOverScreen';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  };
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
-  };
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
-
-  if (userNumber) {
-    screen = <GameScreen />
+    setGameIsOver(false);
+    setGuessRounds(0);
   }
-  
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber && !gameIsOver) {
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+  } else if (gameIsOver && userNumber) {
+    screen = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} />;
+  }
+
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
       <ImageBackground 
-      source={require('../03-MiniGameApp/assets/background.png')} 
-      resizeMode="cover"
-      style={styles.rootScreen}
-      imageStyle={styles.backGroundImage}
+        source={require('../03-MiniGameApp/assets/background.png')} 
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backGroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
