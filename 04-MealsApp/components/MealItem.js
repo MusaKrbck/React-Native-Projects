7f5fafe5d37107b603cea2b3fb40d8f554e0dbb4
MusaKrbck <musakarabacak129@gmail.com>
2024-07-29 19:50:23 +0300

    MealsApp Add Configuring and Details

--- 04-MealsApp/components/MealItem.js
+++ 04-MealsApp/components/MealItem.js
@@ -1,22 +1,40 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native"
+import { View, Text, Pressable, Image, StyleSheet, Platform } from "react-native"
+import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
+function MealItem({ id, title, imageUrl, duration, complexity, affordability }) {
+    const navigation = useNavigation();
 
-function MealItem({title, imageUrl, duration, complexity, affordability }) {
+    function selectMealItemHandler() {
+        navigation.navigate('MealDetail', {
+            mealId: id
+        });
+
+    };
+
+    
     return (
     <View style={styles.mealItem}>
-        <Pressable>
-            <View>
-                <Image 
-                source={{uri: imageUrl}} 
-                style={styles.image}
-                /> 
-                <Text style={styles.title}>{title}</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailItem}>{duration}m</Text>
-                <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-            </View>
+        <Pressable 
+        android_ripple={{color: '#ccc'}} 
+        style= {( { pressed }) => 
+        pressed ? styles.buttonPressed : null}
+        onPress={selectMealItemHandler}
+        >
+            <View style={styles.innerContainer}>
+                <View>
+                    <Image 
+                    source={{uri: imageUrl}} 
+                    style={styles.image}
+                    /> 
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+                <MealDetails 
+                    duration={duration}
+                    affordability={affordability}
+                    complexity={complexity}
+                />
+            </View>        
         </Pressable>
     </View>
 
@@ -30,8 +48,20 @@ const styles = StyleSheet.create({
     mealItem: {
         margin: 16,
         borderRadius: 8,
-        overflow: 'hidden',
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
         backgroundColor : 'white',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.35,
+        shadowOffset: {width: 0, heigh: 2},
+        shadowRadius: 8,
+    },
+    buttonPressed: {
+        opacity: 0.6,
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden',
     },
     image: {
         width: '100%',
@@ -42,15 +72,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         margin: 8,
-    },
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 8,
-    },
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12,
     },
 });
 
