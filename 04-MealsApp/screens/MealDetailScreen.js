7be10d180a74ad51eb7a79bc836c6678b5f2fa10
MusaKrbck <musakarabacak129@gmail.com>
2024-07-30 01:53:53 +0300

    Adding Button Handler

--- 04-MealsApp/screens/MealDetailScreen.js
+++ 04-MealsApp/screens/MealDetailScreen.js
@@ -1,14 +1,28 @@
-import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView, Button } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
-function MealDetailScreen({route}) {
+function MealDetailScreen({route, navigation}) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log("Pressed");
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return  <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />;
+            },
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <View>
