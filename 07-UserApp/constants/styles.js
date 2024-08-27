95ec11c49cfff75c9ad6d645a221cb2506c99622
MusaKrbck <musakarabacak129@gmail.com>
2024-08-27 21:18:33 +0300

    07-UserApp Finished

---
+++ 07-UserApp/constants/styles.js
@@ -0,0 +1,9 @@
+export const Colors = {
+    primary100: '#e0d6f5', // Açık mor
+    primary500: '#8e44ad', // Orta mor
+    primary800: '#5e3370', // Koyu mor
+    error100: '#e8dff0', // Hata için açık mor
+    error500: 'white', // Hata için koyu mor
+    error5000: '#7b1fa2', // Hata için koyu mor
+  }
+  
